d37101affd16a4c74fb1d7580b48a5fdae13bb8f
Andrew Drozdov <andrew@mrdrozdov.com>
2015-04-16 15:46:46 -0400

    R/W Model to File

--- read_model.py
+++ read_model.py
@@ -1,5 +1,3 @@
-# this is my class for making a .txt list of tweets based 
-# on user input of a search word and number of tweets to search 
 
 from tweepy import Stream
 from tweepy import OAuthHandler
@@ -20,10 +18,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF
 import langdetect 
 
-# common_words = open('google-10000-english.txt', 'r').read().splitlines()
-# common_words_set = set(common_words[0:75])
-# print common_words_set
-# q = raw_input("Continue?? ").lower()
+import pickle
 
 filter_prefix_set = ('@', 'http', 'rt', 'www')
 
@@ -70,58 +65,14 @@ def processJsonData(JSON_array):
     # print matrix.shape
 
     return (matrix, feature_names, tweets)
-            
-# create Term Document Matrix for LDA analysis
-def create_TDM(matrix, feature_names):
 
-    # print matrix
-    print type(matrix)
-    print matrix.shape
-   
-    model = lda.LDA(n_topics=40, n_iter=50, random_state=1)
-    model.fit(matrix)
-    print "model fit"
-    topic_word = model.topic_word_  # model.components_ also works
-    n_top_words = 8
-    for i, topic_dist in enumerate(topic_word):
-        topic_words = (feature_names)[np.argsort(topic_dist)][:-n_top_words:-1]
-        print('Topic {}: {}'.format(i, ' '.join(topic_words)))
-
-    # for n in range(10):
-    # topic_most_pr = doc_topic[n].argmax()
-    # print("doc: {} topic: {}\n{}...".format(n,
-    #                                         topic_most_pr,
-    #                                         titles[n][:50]))
-    return model
-
-def matchNewTweet(model, testing_data):
-
-    found_doc = model.transform(testing_data)
-    print "found_doc_shape:" + found_doc.shape
-    print "transformed!!"
-    for title, topics in enumerate(found_doc):
-        print "entered loop!"
-        print("{} (top topic: {})".format(title, topics.argmax()))
-
-
-# write about rate limit data
-def getRateLimit():
-    with open('rate_limit.json', 'w') as outfile:
-        json.dump(api.rate_limit_status(), outfile, sort_keys=True, indent=4)
-
+def log(str):
+    print "\n@@@@@@@@@@@\n"
+    print 
+    print "\n@@@@@@@@@@@\n"
 
 # note: best to interact with user in main function then pass these variable to f(n)
 def main():
-
-
-    #Set Twitter authentication keys
-    ckey = os.environ.get('CKEY')
-    ctoken = os.environ.get('CTOKEN')
-    atoken = os.environ.get('ATOKEN')
-    asecret = os.environ.get('ASECRET')
-
-    auth = OAuthHandler(ckey, ctoken)
-    auth.set_access_token(atoken, asecret)
 
     # convert json into array of statuses
     data = []
@@ -131,32 +82,12 @@ def main():
 
     testing_size = 20
 
-    # training_matrix, feature_names = processJsonData(data[testing_size:1000])
-    # model = create_TDM(training_matrix, feature_names)
-
-    # testing_data, feature_names_2 = processJsonData(data[:testing_size])
-    # matchNewTweet(model, testing_data, feature_names_2)  
-
-    processed_data, feature_names, tword_array = processJsonData(data)  
-
-    print "\n@@@@@@@@@@@\n"
-    print type(processed_data)
-    print "\n@@@@@@@@@@@\n"
+    processed_data, feature_names, tword_array = processJsonData(data[:1000])  
 
     npmatrix = np.array(processed_data.toarray())
 
-    print type(npmatrix)
-
-    model = lda.LDA(n_topics=40, n_iter=50, random_state=1)
-    model.fit(npmatrix[testing_size:])
-
-    # print topics
-    topic_word = model.topic_word_  # model.components_ also works
-    n_top_words = 8
-    for i, topic_dist in enumerate(topic_word):
-        topic_words = (feature_names)[np.argsort(topic_dist)][:-n_top_words:-1]
-        print('Topic {}: {}'.format(i, ' '.join(topic_words)))
-
+    model = pickle.load( open( "awesome.model", "rb" ) )
+    model.transform(npmatrix[:testing_size])
 
     doc_topic_test = model.transform(npmatrix[:testing_size])
 
@@ -167,26 +98,7 @@ def main():
 
     sys.exit(1)
 
-    print processed_data.shape
-    print processed_data[:testing_size]
-    print feature_names
-    # model = create_TDM(processed_data[testing_size:], feature_names)
-    # matchNewTweet(model, processed_data[:testing_size])  
-    
-    # processJsonData()
-
 #standard boilerplate that calls the main() function
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
 
