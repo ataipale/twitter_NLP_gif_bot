1c6f400e15ec831459e3fb96ec71bff21a032a20
Andrew Drozdov <andrew@mrdrozdov.com>
2015-04-23 16:51:37 -0400

    Such clean much joy

--- gen_model.py
+++ gen_model.py
@@ -1,122 +1,18 @@
 
-from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
-from tweepy import Cursor
-import sys
-import os
-import tweepy
-import lda
-import operator
-import pudb
 import json
-import numpy as np
-import re
-import twokenizer
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.decomposition import NMF
-import langdetect 
 
-import pickle
+from awesome_lda import processTweets
+from awesome_lda import countVectorize
+from awesome_lda import getRealWords
+from awesome_lda import getEnglishTweets
+from awesome_lda import processJsonData
+from awesome_lda import trainModel
+from awesome_lda import getTopics
+from awesome_lda import saveModel
 
-filter_prefix_set = ('@', 'http', 'rt', 'www')
 n_top_words = 8
 twitter_file = 'sample_tweets2.json'
 model_file = 'awesome.object'
-
-def processTweets(tweets):
-    """Remove non-english tweets, then stop-words from tweets, and run CountVectorize."""
-
-    english_tweets = getEnglishTweets(tweets)
-    real_tweets = getRealWords(english_tweets)
-    tdm, feature_names = countVectorize(real_tweets)
-
-    return (tdm, feature_names)
-
-def countVectorize(tweets):
-    # use Sklearn's kit to fit and transform the array of just real words from the 
-    # tweets and make TDM
-    count_vectorizer = CountVectorizer(min_df = 2, stop_words = 'english')
-    tdm = count_vectorizer.fit_transform(tweets)
-    feature_names = np.array(count_vectorizer.get_feature_names())
-
-    return (tdm, feature_names)
-
-def getRealWords(tweets):
-    real_tweets = []
-    for tweet_split in tweets:
-        if tweet_split:
-            tokenized = twokenizer.tokenize(tweet_split)
-            # get rid of stopwords
-            # no_stopwords_tweet = [word for word in tokenized if word not in common_words_set]
-            # get rid of punctuation and internet terms
-            just_real_words = [re.sub(r'[^\w\s]','', word) for word 
-                                        in tokenized 
-                                        if not word.startswith(filter_prefix_set)
-                                    ]
-            if just_real_words:
-                real_tweets.append(" ".join(just_real_words))
-
-    return real_tweets
-
-def getEnglishTweets(tweets):
-    english_tweets = []
-    for status in tweets:
-        if status:
-            # ignore non-ascii characters, make all lowercase letters
-            tweet_split = status.encode('ascii', 'ignore').lower()
-            # get rid of useless characters in tweets, and makes words from string
-            if tweet_split:
-                # ignore non-English tweets
-                # print tweet_split
-                try:
-                    lang = langdetect.detect(tweet_split)
-                except langdetect.lang_detect_exception.LangDetectException:
-                    continue
-
-                if lang == 'en':
-                    english_tweets.append(tweet_split)
-
-    return english_tweets
-
-# process and clean the tweets and return a TDM of tweets/vocab and vocab(feature_names)
-def processJsonData(JSON_array):
-    # Return just the text from the tweets represented as JSON data
-    tweets = []
-    for item in JSON_array:
-        status = item.get("text")
-        if status:
-            tweets.append(status)
-
-    # [ item.get("text") if item.get("text") is not None else '' for x in row ]
-
-    return tweets
-
-def trainModel(tdm):
-    npmatrix = np.array(tdm.toarray())
-    model = lda.LDA(n_topics=40, n_iter=50, random_state=1)
-    model.fit(npmatrix)
-    return model
-
-def getTopics(model, feature_names):
-    # topic_word is a matrix of point estimate of word distribution per topic. 
-    # Shape = [n_topics, n_features]
-    topic_word = model.components_ 
-    topic_dict = []
-    
-    for i, word_dist in enumerate(topic_word):
-        print "Here are feature_names:"
-        # Below: For each feature, sort the vocab based on word distribution 
-        # and give me the 8 most relatively important words in each doc
-        topic_words = np.array(feature_names)[np.argsort(word_dist)][:-n_top_words:-1]
-        topic_dict.append(topic_words) 
-        print('Topic {}: {}'.format(i, ' '.join(topic_words)))
-
-    return topic_dict
-
-def saveModel(model, feature_names, topic_dict):
-    pickle_dict = dict(model = model, feature_names = feature_names, topic_dict = topic_dict)
-    pickle.dump( pickle_dict, open(model_file, "wb"))
 
 def main():
 
@@ -130,13 +26,12 @@ def main():
 
     print "[BEGIN] Processing"
     tweets = processJsonData(data[:1000])
-    processed_data_tdm, feature_names = processTweets(tweets)
+    processed_data_tdm, feature_names, _ = processTweets(tweets)
     print "[END] Processing"
 
     model = trainModel(processed_data_tdm)
-    topic_dict = getTopics(model, feature_names)
-    saveModel(model, feature_names, topic_dict)
-    
+    topic_dict = getTopics(model, feature_names, n_top_words)
+    saveModel(model, feature_names, topic_dict, model_file)
 
 #standard boilerplate that calls the main() function
 if __name__ == '__main__':
--- read_model.py
+++ read_model.py
@@ -1,101 +1,8 @@
 import lda
 import json
-import numpy as np
-import re
-import twokenizer
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.decomposition import NMF
-import langdetect 
-import pickle
+import awesome_lda
 
-filter_prefix_set = ('@', 'http', 'rt', 'www')
-
-def processTweets(tweets):
-
-    '''
-    Process and clean the tweets and return a TDM of tweets/vocab and 
-    vocab(feature_names)
-    '''
-
-    tword_array = []
-    clean_tweets = []
-    for status in tweets:
-        tweet_split = status.encode('ascii', 'ignore').lower()
-        # get rid of useless characters in tweets, and makes words from string
-        if tweet_split:
-            # ignore non-English tweets
-            # print tweet_split
-            try:
-                lang = langdetect.detect(tweet_split)
-            except langdetect.lang_detect_exception.LangDetectException:
-                continue
-            if  lang == 'en':
-                # print "entered loop!"
-                tokenized = twokenizer.tokenize(tweet_split)
-                # get rid of stopwords
-                # no_stopwords_tweet = [word for word in tokenized if word not in common_words_set]
-                # get rid of punctuation and internet terms
-                just_real_words = [re.sub(r'[^\w\s]','', word) for word 
-                                            in tokenized 
-                                            if not word.startswith(filter_prefix_set)
-                                            
-                                        ]
-                if just_real_words:
-                    tword_array.append(" ".join(just_real_words))
-                    clean_tweets.append(tweet_split)
-
-    print "[BEGIN] Count Vectorizing"
-    count_vectorizer = CountVectorizer(min_df = 2, stop_words = 'english')
-    matrix = count_vectorizer.fit_transform(tword_array)
-    print "[END] Count Vectorizing"
-
-    feature_names = np.array(count_vectorizer.get_feature_names())
-    print feature_names
-
-    return (matrix, feature_names, clean_tweets)
-
-def processJsonData(JSON_array):
-    tweets = []
-    # parse each tweet to retrieve status and seperate each word
-    for item in JSON_array:
-        # take only status for JSON file
-        status = item.get("text")
-        if status:
-            tweets.append(status)
-
-    return processTweets(tweets)        
-
-def transform_tweet(tweet):
-    return transform_tweets([tweet])
-
-def transform_tweets(tweets):
-
-    '''
-    Transforms Incoming tweet data and returns the tweet and topic
-    '''
-    # Read Model from File
-    model = pickle.load( open( "awesome.object", "rb" ))
-
-    # Prepare Data for Transformation
-    processed_data_TDM, feature_names_newtweet, new_tweets = processJsonData(tweets)  
-    npmatrix = np.array(processed_data_TDM.toarray())
-
-    # Get Parts of Object Dictionary
-    model = model_object.get('model')
-    feature_names_model = model_object.get('feature_names')
-    topic_dict = model_object.get('topic_dict')
-
-    # topic_words is a matrix of point estimate of topic distribution per document 
-    topic_words = model.components_
-
-    # Transform Data against Model
-    doc_topic = model.transform(npmatrix)
-
-    tweet_and_topic = []
-    for title, topic_dist in zip(tweets, doc_topic):
-        tweet_and_topic.append(topic_dict[topic_dist.argmax()], title) 
-
-    return tweet_and_topic
+model_file = "awesome.object" 
 
 # note: best to interact with user in main function then pass these variable to f(n)
 def main():
@@ -104,45 +11,26 @@ def main():
     Read in $LIMIT tweets, and transform them against model that was saved to file
     '''
 
-    limit = 2000
+    limit = 100
 
     # convert json into array of statuses
     data = []
-    n = 0
     with open('sample_tweets2.json') as data_file:
         for line in data_file:
             data.append(json.loads(line))
-            n += 1
-            if n == limit:
-                break
 
     # Read Model from File
-    print "[BEGIN] Reading Onject"
-    model_object = pickle.load(open( "awesome.object", "rb" ))
-    print "[END] Reading Object"
-
-    model = model_object.get('model')
-    feature_names_model = model_object.get('feature_names')
-    topic_dict = model_object.get('topic_dict')
-
-    # topic_words is a matrix of point estimate of word distribution per topic. 
-        # Shape = [n_topics, n_features]
-    topic_words = model.components_
-
-    # Prepare Data for Transformation
-    print "[BEGIN] Processing Data"
-    processed_data_TDM, feature_names_newtweet, tweets = processJsonData(data)  
-    npmatrix = np.array(processed_data_TDM.toarray())
-    print "[END] Processing Data"
+    model, _, topic_dict = awesome_lda.loadModel(model_file)
+    
+    tweets = awesome_lda.processJsonData(data[:limit])
 
     # Transform Data against Model
     # Returns:
         # doc_topic : array-like, shape (n_samples, n_topics)
         # Point estimate of the document-topic distributions
-    doc_topic = model.transform(npmatrix)
+    doc_topic, real_tweets = awesome_lda.transform_tweets(model, tweets)
 
-    for title, topic_dist in zip(tweets, doc_topic):
-        print("(TOPIC {} ::) {} ".format(' '.join(topic_dict[topic_dist.argmax()]), title) )
+    awesome_lda.printTransformedTweets(real_tweets, doc_topic, topic_dict)
 
 if __name__ == '__main__':
     main()
